55dd9f60e5ea2e29f8f66f803d19ec09a041e098
Dylan Roberson <myndphreak1@gmail.com>
2025-04-27 19:15:53 -0400

    Developer tag

--- app.vue
+++ app.vue
@@ -2,10 +2,15 @@
   <ClientOnly>
     <UApp>
       <NuxtLayout>
+	      <div v-if="isDev" class="absolute top-6 left-[-40px] bg-red-600 text-white text-xs font-bold uppercase tracking-wider rotate-[-45deg] w-48 py-2 text-center shadow-lg">
+		      Dev Preview
+	      </div>
           <NuxtPage />
       </NuxtLayout>
     </UApp>
   </ClientOnly>
 </template>
 <script setup lang="ts">
+const isDev = import.meta.env.DEV;
+
 </script>
