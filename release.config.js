6a97f9040793293b30f49fa3cb892655c6ad21ef
Dylan Roberson <mynd@ppo.gg>
2025-05-14 03:44:00 +0000

    Edit release.config.js

--- release.config.js
+++ release.config.js
@@ -1,13 +1,15 @@
-export const branches = ['master'];
-export const plugins = [
-    '@semantic-release/commit-analyzer', // figure out bump type from commits
-    '@semantic-release/release-notes-generator', // generate changelog text
-    '@semantic-release/changelog', // write to CHANGELOG.md
-    '@semantic-release/git', // commit CHANGELOG.md + version bump
-    [
-        '@semantic-release/exec',
-        {
-            publishCmd: 'echo "You could run a deployment script here if needed"',
-        },
+module.exports = {
+    branches: ['master'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/git',
+        [
+            '@semantic-release/exec',
+            {
+                publishCmd: 'echo "You could run a deployment script here if needed"',
+            },
+        ],
     ],
-];
+};
