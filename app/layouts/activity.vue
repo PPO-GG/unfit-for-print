b808636bb6a9d1b6605ca43c46223138e736758a
Dylan Roberson <myndphreak1@gmail.com>
2026-04-06 18:58:45 -0400

    feat(layout): add AppHeader to activity layout with header-height margin

--- app/layouts/activity.vue
+++ app/layouts/activity.vue
@@ -1,8 +1,31 @@
 <template>
-  <div class="relative min-h-screen subpixel-antialiased cursor-default">
+  <div
+    class="relative min-h-screen subpixel-antialiased cursor-default"
+    :style="{ '--ui-header-height': `${headerSize.height}px` }"
+  >
     <ScrollingBackground :gap="12" :scale="0.5" :speedPx="15" />
-    <main class="relative flex-1 p-4">
-      <slot />
-    </main>
+
+    <div class="flex flex-col min-h-screen">
+      <div ref="headerRef">
+        <AppHeader />
+      </div>
+
+      <main
+        :style="{
+          marginTop: `${headerSize.height}px`,
+        }"
+        class="flex-1 p-4 mt-16"
+      >
+        <slot />
+      </main>
+    </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import { useElementSize } from "@vueuse/core";
+
+const headerRef = ref(null);
+const headerSize = useElementSize(headerRef);
+</script>
