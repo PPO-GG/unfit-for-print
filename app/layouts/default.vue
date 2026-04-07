8336a0aa3e00c2bd53b7e8313f7067bc347559a5
Dylan Roberson <myndphreak1@gmail.com>
2026-04-07 18:55:55 -0400

    fix(layout): add AppHeader to default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,6 +2,7 @@
   <div class="min-h-screen relative subpixel-antialiased cursor-default">
     <ScrollingBackground :gap="12" :scale="0.5" :speedPx="15" />
     <AtmosphericOverlay vignette />
+    <AppHeader />
     <main class="relative z-10 min-h-screen flex flex-col">
       <slot />
     </main>
